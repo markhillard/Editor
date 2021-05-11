3ee683c274b39d274d9acf4aa3b2e73ab9e09b1c
Mark Hillard <markhillard@gmail.com>
2021-05-10 22:04:20 -0400

    Update default HTML code pane value

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 █████   ██   ██ ██    ██    ██    ██ ██████
 ██      ██   ██ ██    ██    ██    ██ ██   ██
 ███████ ██████  ██    ██     ██████  ██   ██
-2019 ~ Mark Hillard | (mark@)markhillard.com
+2021 ~ Mark Hillard | (mark@)markhillard.com
 */
 
 
@@ -177,7 +177,7 @@ E(document).ready(function () {
     
     // DEFAULTS
     // ------------------------------
-    var defaultHTML = '<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js\"></script>\n<main>\n    <h1>Editor</h1>\n    <p>Real-time, responsive HTML/CSS/JS code editor</p>\n    <p>Fork me on <a href=\"https://github.com/markhillard/Editor\" target=\"_blank\">GitHub</a></p>\n</main>',
+    var defaultHTML = '<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js\"></script>\n<main>\n    <h1>Editor</h1>\n    <p>Real-time, responsive HTML/CSS/JS code editor</p>\n    <p>Fork me on <a href=\"https://github.com/markhillard/Editor\" target=\"_blank\">GitHub</a></p>\n</main>',
         defaultCSS = '@import url(\"https://fonts.googleapis.com/css?family=Fira+Code:400,700\&display=swap\");\n\nhtml,body {\n    background-color: #282a36;\n    color: #fff;\n    font-family: \"Fira Code\", monospace;\n    font-weight: 400;\n    overflow: hidden;\n    text-align: center;\n}\n\nmain {\n    left: 50%;\n    position: absolute;\n    top: 50%;\n    transform: translate(-50%, -50%);\n    width: 80%;\n}\n\nh1 {\n    font-size: 8rem;\n    font-weight: 700;\n    margin: 0;\n}\n\np {\n    font-size: 1rem;\n    letter-spacing: .03rem;\n    line-height: 1.45;\n    margin: 1rem 0;\n}\n\na {\n    color: #6d8a88;\n}\n\n@media only screen and (max-width: 600px) {\n    h1 {\n        font-size: 4rem;\n    }\n}',
         defaultJS = '$(document).ready(function () {\n    $(\'h1\').fadeOut(800).fadeIn(800);\n    $(\'p\').first().delay(400).fadeOut(800).fadeIn(400);\n    $(\'p\').last().delay(800).fadeOut(800).fadeIn(400);\n});',
         defaultFontSize = '100';
